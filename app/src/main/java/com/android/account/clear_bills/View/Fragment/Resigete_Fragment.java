07a5a1edcda01eb85f56902221d2806a66324000
mutoujun <824532767@qq.com>
2017-11-19 17:07:05 +0800

    1

--- app/src/main/java/com/android/account/clear_bills/View/Fragment/Resigete_Fragment.java
+++ app/src/main/java/com/android/account/clear_bills/View/Fragment/Resigete_Fragment.java
@@ -1,25 +1,30 @@
 package com.android.account.clear_bills.View.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.android.account.clear_bills.View.MainActivity;
+import com.android.account.clear_bills.Interface.Bmob_Login_interface;
 import com.android.account.clear_bills.R;
+import com.android.account.clear_bills.ViewModel.Bmob_Net;
 
 /**
  * Created by Administrator on 2017/11/18.
  */
 
 public class Resigete_Fragment extends Fragment implements View.OnClickListener{
+    EditText name,account,password,repassword;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,6 +41,12 @@ public class Resigete_Fragment extends Fragment implements View.OnClickListener{
         actionBar.setTitle("");
         ImageView imageBack = (ImageView) view.findViewById(R.id.img_back);
         imageBack.setOnClickListener(this);
+        Button encrollButton = (Button) view.findViewById(R.id.btn_encroll);
+        encrollButton.setOnClickListener(this);
+        name = (EditText) view.findViewById(R.id.enroll_name);
+        account = (EditText) view.findViewById(R.id.enroll_account);
+        password = (EditText) view.findViewById(R.id.enroll_passwrd);
+        repassword = (EditText) view.findViewById(R.id.enroll_repasswrd);
     }
 
 
@@ -45,6 +56,25 @@ public class Resigete_Fragment extends Fragment implements View.OnClickListener{
             case R.id.img_back:
                 getFragmentManager().popBackStack();
                 break;
+            case R.id.btn_encroll:
+                if(TextUtils.isEmpty(name.getText())||TextUtils.isEmpty(password.getText())||
+                        TextUtils.isEmpty(account.getText())||TextUtils.isEmpty(repassword.getText())){
+                    Toast.makeText(getContext(), "信息不完整", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!password.getText().toString().equals(repassword.getText().toString())){
+                    Toast.makeText(getContext(), "两次密码不相等", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Bmob_Net.getBmob_net(getContext()).register(name.getText().toString(), account.getText().toString(), password.getText().toString(), new Bmob_Login_interface() {
+                    @Override
+                    public void success(int code, String message) {
+                        if(code==1){
+                            getFragmentManager().popBackStack();
+                        }
+                        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                    }
+                });
         }
     }
 }
