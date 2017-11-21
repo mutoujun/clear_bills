131d0e42535f5707c3e3a40779bbe971cde349e3
mutoujun <824532767@qq.com>
2017-11-21 19:39:51 +0800

    2017/11/21

---
+++ app/src/main/java/com/android/account/clear_bills/Interface/Filesave.java
@@ -0,0 +1,19 @@
+package com.android.account.clear_bills.Interface;
+
+/**
+ * Created by Administrator on 2017/11/21.
+ */
+
+public interface Filesave {
+    /**记住密码
+     * 登录成功调用
+     * @param b 填入是否记住密码
+     */
+    void savepassword(boolean b);
+
+    /**获取密码
+     * 打开软件调用，接口回调密码在success中
+     * @param callback
+     */
+    void getpassword(Callback<String> callback);
+}
